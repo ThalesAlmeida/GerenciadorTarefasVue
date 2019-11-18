ac92c28db5e5fe14ba9dfc355d36b48e63769e0e
Thales <thalesalves44@gmail.com>
2019-11-17 21:27:11 -0300

    Init

---
+++ src/components/TarefasListaIten.vue
@@ -0,0 +1,60 @@
+<template>
+    <li class="list-group-item d-flex">
+        <span>{{ tarefa.titulo }}</span>
+        <span class="espacar"></span>
+        <button 
+        @click="concluirTarefa"
+        class="btn btn-sm mr-4" :class="classeCSS" :title="tituloBotaoConcluido">
+            <i class="fa fa-check"></i>
+        </button>
+        <button 
+        @click="$emit('editar', tarefa)"
+        class="btn btn-primary btn-sm mr-1" title="Editar">
+            <i class="fa fa-pencil-alt"></i>
+        </button>
+        <button 
+        @click="$emit('deletar', tarefa)"
+        class="btn btn-danger btn-sm" title="Deletar">
+            <i class="fa fa-trash"></i>
+        </button>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        tarefa: {
+            type: Object,
+            requred: true
+        }
+    },
+    computed: {
+        classeCSS() {
+            return {
+                'btn-secondary': !this.tarefa.concluido,
+                'btn-success': this.tarefa.concluido
+            }
+        },
+        tituloBotaoConcluido() {
+            return this.tarefa.concluido
+                ? 'Refazer Tarefa'
+                : 'Concluir Tarefa'
+        },
+
+    },
+    methods:{
+        concluirTarefa(){
+            this.$emit('concluir', Object.assign({}, this.tarefa, {concluido: !this.tarefa.concluido}))
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+    .espacar {
+        flex: 1 1 auto;
+    }
+
+</style>
+
